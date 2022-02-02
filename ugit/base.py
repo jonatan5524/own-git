1afb1d1e7902f50ca0e8aa685b8c331912c2e3f6
jonatan <jonatan5524@gmail.com>
2022-02-02 15:36:36 +0200

    ugit add read-tree

--- ugit/base.py
+++ ugit/base.py
@@ -1,11 +1,12 @@
 import os
+from typing import Dict, Iterator, Tuple
 
 from numpy import full
 
 from . import data
 
 
-def write_tree(directory: str = "."):
+def write_tree(directory: str = ".") -> str:
     entries = []
     with os.scandir(directory) as dir:
         for entry in dir:
@@ -19,7 +20,7 @@ def write_tree(directory: str = "."):
 
                 with open(full_path, "rb") as f:
                     object_id = data.hash_object(f.read())
-										
+
             elif entry.is_dir(follow_symlinks=False):
                 fmt = "tree"
                 object_id = write_tree(full_path)
@@ -32,5 +33,43 @@ def write_tree(directory: str = "."):
     return data.hash_object(tree.encode(), "tree")
 
 
-def is_ignored(path: str):
+def is_ignored(path: str) -> bool:
     return ".ugit" in path.split("/")
+
+
+def read_tree(tree_object_id: str):
+    for path, object_id in get_tree(tree_object_id, base_path="./").items():
+        os.makedirs(os.path.dirname(path), exist_ok=True)
+
+        with open(path, "wb") as f:
+            f.write(data.get_object(object_id))
+
+
+def get_tree(object_id: str, base_path: str = "") -> Dict[str, str]:
+    result = {}
+
+    for fmt, object_id, name in _iter_tree_entries(object_id):
+        assert "/" not in name
+        assert name not in ("..", ".")
+
+        path = base_path + name
+
+        if fmt == "blob":
+            result[path] = object_id
+        elif fmt == "tree":
+            result.update(get_tree(object_id, f"{path}/"))
+        else:
+            assert False, f"Uknown tree entry {fmt}"
+
+    return result
+
+
+def _iter_tree_entries(object_id: str) -> Iterator[Tuple[str, str, str]]:
+    if not object_id:
+        return
+
+    tree = data.get_object(object_id, "tree")
+
+    for entry in tree.decode().splitlines():
+        fmt, object_id, name = entry.split(" ", 2)
+        yield fmt, object_id, name
--- ugit/cli.py
+++ ugit/cli.py
@@ -32,6 +32,10 @@ def parse_args():
     write_tree_parser = commands.add_parser("write-tree")
     write_tree_parser.set_defaults(func=write_tree)
 
+    read_tree_parser = commands.add_parser("read-tree")
+    read_tree_parser.set_defaults(func=read_tree)
+    read_tree_parser.add_argument("tree")
+
     return parser.parse_args()
 
 
@@ -53,3 +57,7 @@ def cat_file(args: argparse.Namespace):
 
 def write_tree(args: argparse.Namespace):
     print(base.write_tree())
+
+
+def read_tree(args: argparse.Namespace):
+    base.read_tree(args.tree)
